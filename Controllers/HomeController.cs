18348c96f494a6f82ac5a0dafaba394dce096300
Dua!@#067 <duaynne@duacro.co.za>
2023-05-12 16:13:44 +0200

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Inde()
+    public IActionResult Index()
     {
         return View();
     }

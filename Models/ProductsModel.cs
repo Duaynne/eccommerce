05fe609b540bdb9c58851e3c68f0bf1d6a941a05
Dua!@#067 <duaynne@duacro.co.za>
2023-05-14 07:43:58 +0200

    Update

--- Models/ProductsModel.cs
+++ Models/ProductsModel.cs
@@ -2,15 +2,9 @@ namespace MVC1.Models;
 
 public class ProductsViewModel
 {
-public string? Name { get; set; }
-public string? Desc { get; set; }
-public string? Price { get; set; }
-public string? Qty { get; set; }
-public int? QtySelected { get; set; }
-public string? ImgSource { get; set; }
-public string? ProdID { get; set; }
-public string? CatID { get; set; }
-public string? CategoryName { get; set; }
-public string? FileName { get; set; }
-public IFormFile? file { get; set; }
+public string? ProductID { get; set; }
+public string? ProductName { get; set; }
+public string? ProductDescription { get; set; }
+public double? Price { get; set; }
+public int? Qty { get; set; }
 }
--- Models/ShoppingModel.cs
+++ Models/ShoppingModel.cs
@@ -2,7 +2,16 @@ namespace MVC1.Models;
 
 public class ShoppingViewModel
 {
-    public string? RequestId { get; set; }
+    public class order
+    {
+        public int? OrderID { get; set; }
+        public int? CustomerID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? ShippingAddress { get; set; }
+        public string? BillingAddress { get; set; }
+        public Double? Total { get; set; }
+    }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        
+    
 }
